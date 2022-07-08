36c804c4ee38df39c4b33245b601f27d572549c6
Vetrivel2505 <vetrivels2505@gmail.com>
2022-07-08 10:08:05 +0530

    First Commit for calulator

---
+++ main.js
@@ -0,0 +1,24 @@
+function add()
+{
+    var result = Number(document.getElementById("firstno").value)+
+    Number(document.getElementById("secondno").value);
+    window.alert(result)
+}
+function sub()
+{
+    var result = Number(document.getElementById("firstno").value)-
+    Number(document.getElementById("secondno").value);
+    window.alert(result)
+}
+function mul()
+{
+    var result = Number(document.getElementById("firstno").value)*
+    Number(document.getElementById("secondno").value);
+    window.alert(result)
+}
+function div()
+{
+    var result = Number(document.getElementById("firstno").value)/
+    Number(document.getElementById("secondno").value);
+    window.alert(result)
+}
